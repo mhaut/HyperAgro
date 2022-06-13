ac801de1c91fb5be461fc88ba82785c387639ba9
sgarciatz <sgarciatz@alumnos.unex.es>
2022-06-13 16:40:14 +0200

    Correccion de errores

--- todoJunto/utils.py
+++ todoJunto/utils.py
@@ -147,7 +147,8 @@ def get_image_hyper(credentials_path,source_folder, dest_folder, capture_timesta
 def delete_images(source_folder, max_ttl, logger):
     for image in os.listdir(source_folder):
         file_path = os.path.join(source_folder, image)
-        file_deltatime = datetime.now() - os.getmtime(file_path)
+        file_deltatime = datetime.now() - datetime.fromtimestamp(os.path.getmtime(file_path))
         if file_deltatime > max_ttl:
             #os.remove(os.path.join(source_folder,image))
             logger.info("fichero " +  file_path.split("/")[-1] + " listo para ser borrado")
+
